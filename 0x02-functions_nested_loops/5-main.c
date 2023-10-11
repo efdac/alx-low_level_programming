73c35c04db5589027120fc2afaa10771fc59add3
efdac <emanmohsen800@gmail.com>
2023-10-11 16:03:08 -0700

    r

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-    int r;
-
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
+	int r;
+	r = print_sign(98);
+	_putchar(',');
+       	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
