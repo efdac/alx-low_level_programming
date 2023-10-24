799c49c334e67a65bbd5cfedbdf170f401ec45c1
efdac <emanmohsen800@gmail.com>
2023-10-23 17:23:34 -0700

    this is a commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include "main.h"
-/**
- *_memset - The _memset() function fills
- * @s: pointer that make you but the char
- * @b: the char you want to repeted it constant
- * @n: next byet
- *Return: returns new value of target
- */
-char *_memset(char *s, char b, unsigned int n)
-{	unsigned int i;
-	for (i = 0 ; n > 0; i++, n--)
-	{
-		s[i] = b;
-=======
 /*
  * File: 0-memset.c
  * Auth: not
@@ -37,7 +21,7 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[n - 1] = b;
 		n--;
->>>>>>> ecf1902c0c2978c1c59957cf20ed50a2c933c73a
 	}
 	return (s);
 }
+
